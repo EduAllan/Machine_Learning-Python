be0fdeca2e8b1354e14cf209e9331cb1a4b6ebd8
eduardo <eduardo.lucio.correia@gmail.com>
2016-02-19 13:43:05 -0300

    Criação da classe Instance.

--- projeto/instancia.py
+++ projeto/instancia.py
@@ -1,1 +1,20 @@
 __author__ = 'allan'
+
+class Instance(object):
+    num_columns = []
+    str_columns = []
+    instance_class = ""
+
+    def __init__(self,line,header):
+        columns = str.split()
+        for i,j in zip(line,header):
+            if(j == "num"):
+                self.num_columns.append(i);
+            if(j == "str"):
+                self.str_columns.append(i);
+            if(j == "class"):
+                self.instance_class = i;
+
+    def normalize(self,min,max):
+        for i,j,k in zip(min,max,self.num_columns):
+            k = (k-i)/(j-i)
