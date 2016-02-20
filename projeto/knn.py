8a75c663b85a2744c7e6f9d2d69dbe704c393060
allan <luciocorreia.allan@gmail.com>
2016-02-20 10:04:32 -0300

    a

--- projeto/knn.py
+++ projeto/knn.py
@@ -28,5 +28,5 @@ class KNN(object):
 
 teste = Instances('entrada.txt')
 classifier = KNN(teste,3)
-input = Instance([6.5,3.0,5.2,2.0],["num","num","num","num"])
+input = Instance([10,1.30,60],["num","num","num"])
 print classifier.classify(input)
